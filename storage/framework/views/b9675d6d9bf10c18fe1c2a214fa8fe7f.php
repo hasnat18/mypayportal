d159adc80feb3fad67a163ad430ccc8e969d413e
hasnat18 <hsntkhan1614@gmail.com>
2024-04-29 03:30:33 +0500

    new

--- storage/framework/views/b9675d6d9bf10c18fe1c2a214fa8fe7f.php
+++
@@ -1,6 +0,0 @@
-<?php $__env->startSection('content'); ?>
-
-    @livewire('customer-manager')
-
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Hasnat Khan\Desktop\mypayportal\mypayportal\resources\views\customer-management.blade.php ENDPATH**/ ?>
